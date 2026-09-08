cd8dc501525016338d9f26f9e21df0c5b16d9bfc
Roky777 <paulroky78@gmail.com>
2026-09-08 17:01:28 +0530

    Use unique Grade 1 English progress storage key

--- progress files/config.js
+++ progress files/config.js
@@ -23,7 +23,7 @@ const CONFIG = {
 
   // Storage Configuration
   storage: {
-    storageKey: 'brainMatch-periodic_progress',
+    storageKey: 'brainMatch-grade1-english_progress',
     useAsyncStorage: false, // Set to true for React Native
   },
 
